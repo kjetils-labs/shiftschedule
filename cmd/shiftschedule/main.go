2ab0757eaafd9ac0773d67c5a8c716e1a75fb996
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2025-11-13 15:34:30 +0100

    feature: tested minimum setup

--- cmd/shiftschedule/main.go
+++ cmd/shiftschedule/main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/shiftschedule/internal/api"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/shiftschedule/internal/clients/postgres"
+	"github.com/shiftschedule/test"
 )
 
 func main() {
 
+	url := "postgres://user:Password123@localhost:5432/shiftschedule"
 	ctx := context.Background()
-	api.InitHttpServer(ctx)
+	config, err := pgxpool.ParseConfig(url)
+	config.ConnConfig.TLSConfig = nil
+	if err != nil {
+		panic(fmt.Errorf("failed to parse config from url %v. %w", url, err))
+	}
+	pg, err := postgres.Init(ctx, config)
+	if err != nil {
+		panic(fmt.Errorf("failed to start postgres init. %w", err))
+	}
+
+	err = test.InitTestData(ctx, pg)
+	if err != nil {
+		panic(fmt.Errorf("failed to init test data. %w", err))
+	}
+	// api.InitHttpServer(ctx)
 }
